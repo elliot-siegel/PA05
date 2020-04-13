5d855352c8cd88b71ed2174e5fbf511b64eba8a6
elliot-siegel <62267188+elliot-siegel@users.noreply.github.com>
2020-04-13 16:10:39 -0400

    Added comments

--- src/lesson19/Compromised.java
+++ src/lesson19/Compromised.java
@@ -1,6 +1,10 @@
 package lesson19;
 import java.util.Random;
-
+/**
+* This class simulates a person who is immunocompromised.
+* They are more likely to get infected and they recover slower,
+* But they are more cautious so they are less likely to move around.
+*/
 public class Compromised extends Person {
 
   public Compromised(){

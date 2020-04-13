f4f05098fa7912722d634c7beac6d5d14881e3ca
elliot-siegel <elliotsiegel@brandeis.edu>
2020-04-12 22:58:51 -0400

    files

--- PA05-master/src/lesson19/Personfile.java
+++
@@ -1,17 +0,0 @@
-package lesson19;
-import java.util.Random;
-
-public class Personfile extends Person{
-
-  public Personfile(){
-    super();
-    infectionProb = 0.75;
-    recoveryTime = 7;
-  }
-  public void tryToMove(){
-    Random randInt = new Random();
-    if(randInt.nextInt(5) == 0){ 
-      tryToMoveRandomly();
- }
-  }
-}
